c206f207ea71a5ba2f77627fc2832d99ad5f2eac
Matt Pan <matthewmpan@gmail.com>
2025-07-10 11:58:43 -0700

    changed /learn-more route to /about

--- core/components/footer/info-section.tsx
+++ core/components/footer/info-section.tsx
@@ -19,7 +19,7 @@ export default function InfoSection() {
       <div>
         <Link
           className="my-3 flex items-center justify-center gap-1 font-bold text-pink-500 md:justify-start"
-          href="/learn-more"
+          href="/about"
         >
           Learn More <FiArrowRight />
         </Link>
