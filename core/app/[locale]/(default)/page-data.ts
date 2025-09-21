34c4cb1d0d83428418fb176491afd57c845ccc8b
Vardan <vardan.vanyan@gmail.com>
2025-09-20 23:22:47 -0700

    create new query for banner data from bigcommerce gql

--- core/app/[locale]/(default)/page-data.ts
+++ core/app/[locale]/(default)/page-data.ts
@@ -109,3 +109,32 @@ export const getPopularProductsData = cache(
     return data;
   },
 );
+
+const BannersQuery = graphql(`
+  query BannersQuery {
+    site {
+      banners {
+        homePage(first: 10) {
+          edges {
+            node {
+              entityId
+              name
+              content
+              location
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const getBannersData = cache(async (customerAccessToken?: string) => {
+  const { data } = await client.fetch({
+    document: BannersQuery,
+    customerAccessToken,
+    fetchOptions: customerAccessToken ? { cache: 'no-store' } : { next: { revalidate } },
+  });
+
+  return data;
+});
